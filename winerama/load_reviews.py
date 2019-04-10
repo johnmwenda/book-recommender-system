377c32d0a270c0824d7b8b33fd0a7234cc191d65
John Mwenda <jmwenda56@gmail.com>
2019-04-10 20:27:46 +0300

    load data into the database using pandas and django framework

--- winerama/load_reviews.py
+++ winerama/load_reviews.py
@@ -1,13 +1,14 @@
-import sys, os 
+import django
+import sys
+import os
 import pandas as pd
 import datetime
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "winerama.settings")
 
-import django
 django.setup()
 
-from reviews.models import Review, Wine 
+from reviews.models import Review, Wine  # nopep8
 
 
 def save_review_from_row(review_row):
@@ -19,21 +20,21 @@ def save_review_from_row(review_row):
     review.pub_date = datetime.datetime.now()
     review.comment = review_row[4]
     review.save()
-    
-    
+
+
 if __name__ == "__main__":
-    
+
     if len(sys.argv) == 2:
-        print "Reading from file " + str(sys.argv[1])
+        print("Reading from file " + str(sys.argv[1]))
         reviews_df = pd.read_csv(sys.argv[1])
-        print reviews_df
+        print(reviews_df)
 
         reviews_df.apply(
             save_review_from_row,
             axis=1
         )
 
-        print "There are {} reviews in DB".format(Review.objects.count())
-        
+        print("There are {} reviews in DB".format(Review.objects.count()))
+
     else:
-        print "Please, provide Reviews file path"
+        print("Please, provide Reviews file path")
