09f78b9ce978afe95640eb14110a75f57dee6b22
John Mwenda <jmwenda56@gmail.com>
2019-04-10 22:29:22 +0300

    create user recommendation view, which uses cluster information to determine suggestions to a user

--- winerama/reviews/views.py
+++ winerama/reviews/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from .models import Review, Wine
+from .models import Review, Wine, Cluster
+from django.contrib.auth.models import User
 from .forms import ReviewForm
 import datetime
 
 from django.contrib.auth.decorators import login_required
 
+
 def review_list(request):
     latest_review_list = Review.objects.order_by('-pub_date')[:9]
-    context = {'latest_review_list':latest_review_list}
+    context = {'latest_review_list': latest_review_list}
     return render(request, 'reviews/review_list.html', context)
 
 
@@ -20,7 +22,7 @@ def review_detail(request, review_id):
 
 def wine_list(request):
     wine_list = Wine.objects.order_by('-name')
-    context = {'wine_list':wine_list}
+    context = {'wine_list': wine_list}
     return render(request, 'reviews/wine_list.html', context)
 
 
@@ -28,6 +30,7 @@ def wine_detail(request, wine_id):
     wine = get_object_or_404(Wine, pk=wine_id)
     form = ReviewForm()
     return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
+
 
 @login_required
 def add_review(request, wine_id):
@@ -48,14 +51,46 @@ def add_review(request, wine_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('reviews:wine_detail', args=(wine.id,)))
-    
+
     return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
-    
+
 
 def user_review_list(request, username=None):
     if not username:
         username = request.user.username
     latest_review_list = Review.objects.filter(user_name=username).order_by('-pub_date')
-    context = {'latest_review_list':latest_review_list, 'username':username}
+    context = {'latest_review_list': latest_review_list, 'username': username}
     return render(request, 'reviews/user_review_list.html', context)
 
+
+@login_required
+def user_recommendation_list(request):
+    # get this user reviews
+    user_reviews = Review.objects.filter(user_name=request.user.username).prefetch_related('wine')
+    user_reviews_wine_ids = set(map(lambda x: x.wine.id, user_reviews))
+
+    # get request user name (just the first one right now)
+    user_cluster_name = User.objects.get(username=request.user.username).cluster_set.first().name
+
+    # get usernames for other members of the cluster
+    user_cluster_other_members = Cluster.objects.get(
+        name=user_cluster_name).users.exclude(username=request.user.username).all()
+    other_members_usernames = set(map(lambda x: x.username, user_cluster_other_members))
+
+    # get reviews by those users, excluding wines reviewed by the request user_reviews
+    other_users_reviews = Review.objects.filter(
+        user_name__in=other_members_usernames).exclude(wine_id__in=user_reviews_wine_ids)
+    other_users_reviews_wine_ids = set(map(lambda x: x.wine.id, other_users_reviews))
+
+    # then get a wine list excluding the previous IDs
+    wine_list = sorted(
+        list(Wine.objects.filter(id__in=other_users_reviews_wine_ids)),
+        key=lambda x: x.average_rating(),
+        reverse=True
+    )
+
+    return render(
+        request,
+        'reviews/user_recommendation_list.html',
+        {'username': request.user.username, 'wine_list': wine_list}
+    )
